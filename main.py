2a5d01785983e4d36254e089e4cd3c3868266c56
LikaPashteta <fllika999@gmail.com>
2026-03-09 19:54:20 +0300

    Update main.py

--- main.py
+++ main.py
@@ -9,47 +9,96 @@ def start(message):
     markup.add(btn1)
     bot.send_message(message.from_user.id, "Скоро тут будет история, пока воть", reply_markup=markup)
 
+user_names = {}  # словарь для имён
+
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message):
-    #первые кнопки, ознакомление с правилами и начало игры
+    user_id = message.from_user.id  # Получаем ID пользователя
+
+    # Первые кнопки, ознакомление с правилами и начало игры
     if message.text == 'Начать':
-        markup = types.ReplyKeyboardMarkup(resize_keyboard=True) #создание новых кнопок
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)  # Создание новых кнопок
         btn1 = types.KeyboardButton('Помощь')
         btn2 = types.KeyboardButton('Правила пользования')
         btn3 = types.KeyboardButton('Начать приключение')
         markup.add(btn1, btn2, btn3)
-        bot.send_message(message.from_user.id, 'Задайте интересующий вопрос', reply_markup=markup) #ответ бота
+        bot.send_message(message.from_user.id, 'Задайте интересующий вопрос', reply_markup=markup)  # Ответ бота
+
     elif message.text == 'Помощь':
         bot.send_message(message.from_user.id, 'Здесь будет пара кнопок перехода в инфо канал', parse_mode='Markdown')
+
     elif message.text == 'Правила пользования':
-        bot.send_message(message.from_user.id, 'Подробно вы можете ознакомиться с правилами ' + '[тут](https://telegra.ph/Pravila-polzovaniya-bota-11-11)', parse_mode='Markdown')
-    #вторая кнопка, даём персонажу имя
-    if message.text == 'Начать приключение':
-        markup = types.ReplyKeyboardMarkup(resize_keyboard=True) #создание новых кнопок
+        bot.send_message(message.from_user.id, 'Подробно вы можете ознакомиться с правилами ' + '[тут](https://telegra.ph/Pravila-polzovaniya-botom-03-09)', parse_mode='Markdown')
+
+    # Вторая кнопка, даём персонажу имя
+    elif message.text == 'Начать приключение':
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)  # Создание новых кнопок
         btn1 = types.KeyboardButton('Назвать персонажа')
         markup.add(btn1)
-        bot.send_message(message.from_user.id, 'Дайте своему герою имя', reply_markup=markup) #ответ бота
-#Начиная с этого момента код не работает, пока разбираюсь как дать ему текст, чтобы он его запомнил и не забывал пользователя
-    if message.text == 'Дайте своему герою имя':
-        markup = types.ReplyKeyboardMarkup(resize_keyboard=True) #создание новых кнопок
-        btn1 = types.KeyboardButton('Username')
-        markup.add(btn1)
-    elif message.text == 'Username':
-        bot.send_message(message.chat.id, 'Ваше имя будет: ' + message.text, parse_mode='Markdown')
-    
-    if message.text == 'Ваше имя будет: ':
-        markup = types.ReplyKeyboardMarkup(resize_keyboard=True) #создание новых кнопок
-        btn1 = types.KeyboardButton('Все верно')
-        btn1 = types.KeyboardButton('Переделаем')
-        markup.add(btn1, btn2)
-    elif message.text == 'Переделаем':
-        bot.send_message(message.chat.id, 'Ваше имя будет: ' + message.text, parse_mode='Markdown') #ответ бота
-    
-    if message.text == 'Все верно':
-        markup = types.ReplyKeyboardMarkup(resize_keyboard=True) #создание новых кнопок
-        btn1 = types.KeyboardButton('Начнем игру')
+        bot.send_message(message.from_user.id, 'Дайте своему герою имя', reply_markup=markup)  # Ответ бота
 
-bot.polling(none_stop=True, interval=0) #обязательная для работы бота часть
+    elif message.text == 'Назвать персонажа':
+        bot.send_message(message.from_user.id, 'Введите имя вашего персонажа')
+
+    # Сохраняем введенное имя в словарь
+    elif user_id not in user_names:
+        user_names[user_id] = message.text  # Сохраняем имя пользователя
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)  # Создание новых кнопок
+        btn1 = types.KeyboardButton('Все верно')
+        markup.add(btn1)
+        bot.send_message(message.from_user.id, f'Ваше имя будет: {user_names[user_id]}', reply_markup=markup)
+
+    elif message.text == 'Все верно':
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)  # Создание новых кнопок
+        btn1 = types.KeyboardButton('Начнем игру')
+        markup.add(btn1)
+        bot.send_message(message.from_user.id, 'Имя сохранено. Нажмите "Начнем игру", чтобы продолжить.', reply_markup=markup)
+
+    elif message.text == 'Начнем игру':
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        btn1 = types.KeyboardButton('Выбрать расу')
+        markup.add(btn1)
+        bot.send_message(message.from_user.id, f'Игра началась! Ваше имя: {user_names[user_id]}', reply_markup=markup)
+
+    elif message.text == 'Выбрать расу':
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        btn1 = types.KeyboardButton('Человек')
+        btn2 = types.KeyboardButton('Эльф')
+        btn3 = types.KeyboardButton('Гоблин')
+        markup.add(btn1, btn2, btn3)
+        
+    elif message.text == 'Человек':
+        bot.send_message(message.from_user.id, 'Отлично! Ваша раса: Человек', parse_mode='Markdown')
+    elif message.text == 'Эльф':
+        bot.send_message(message.from_user.id, 'Отлично! Ваша раса: Эльф', parse_mode='Markdown')
+    elif message.text == 'Гоблин':
+        bot.send_message(message.from_user.id, 'Отлично! Ваша раса: Гоблин', parse_mode='Markdown')
+
+    elif message.text == 'Человек' or 'Эльф' or 'Гоблин':
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        btn1 = types.KeyboardButton('2 часа')
+        btn2 = types.KeyboardButton('6 часов')
+        btn3 = types.KeyboardButton('12 часов')
+        markup.add(btn1, btn2, btn3)
+        bot.send_message(message.from_user.id, 'Выберите время отправки отчёта о вашем персонаже', reply_markup=markup)
+
+    elif message.text == '2 часа' or '6 часов' or '12 часов':
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        btn1 = types.KeyboardButton('ДА!')
+        btn2 = types.KeyboardButton('Нет...')
+        markup.add(btn1, btn2)
+        bot.send_message(message.from_user.id, 'Отлично, время выбрано! Начинаем!', reply_markup=markup)
+
+    elif message.text == 'Нет...':
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        bot.send_message(message.from_user.id, 'Ну нет так нет, до встречи)', reply_markup=markup)
+        
+    elif message.text == 'ДА!':
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        bot.send_message(message.from_user.id, 'Отлично, начинаем!', reply_markup=markup)
+        
+bot.polling(none_stop=True, interval=0) #чтобы бот работал бесконечно
+
 
 
 
